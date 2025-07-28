b246a2050135de6fdaf42de5b1221bf9eaf4220a
Douglas Lima <douglaslima2363@gmail.com>
2025-07-28 17:49:17 -0300

    correção para copilar

--- src/main/java/com/teste/HcodeR/domain/Beneficio.java
+++ src/main/java/com/teste/HcodeR/domain/Beneficio.java
@@ -3,11 +3,12 @@ package com.teste.HcodeR.domain;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
+
 
 @Entity
 @Data
